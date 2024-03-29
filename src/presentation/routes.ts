b22dcdfe20f6481eb9e15fbe40f2cdf09e5e0401
Bryan Joseph <bryanllana14@gmail.com>
2024-03-29 09:35:46 -0500

    Swagger config added

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { ProductRoutes } from "./products";
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec from "../config/swagger";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router()
 
     router.use('/api/products', ProductRoutes.routes)
+    router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
     return router
   }
